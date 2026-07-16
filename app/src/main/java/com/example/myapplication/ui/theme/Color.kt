426b5d2b9784e2526eab103d451f139f6bc9c02d
TRISTAN JOULES C. PAHAYAHAY <tjpahayahay67260@liceo.edu.ph>
2026-07-16 14:10:34 +0800

    acticity-2

--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package com.example.myapplication.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryLight = Color(0xFF1E3A8A)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryContainerLight = Color(0xFFDBEAFE)
+val SecondaryLight = Color(0xFF3B82F6)
+val SurfaceLight = Color(0xFFFFFFFF)
+val BackgroundLight = Color(0xFFF8FAFC)
+val OnSurfaceVariantLight = Color(0xFF475569)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryDark = Color(0xFF60A5FA)
+val OnPrimaryDark = Color(0xFF0F172A)
+val PrimaryContainerDark = Color(0xFF1E40AF)
+val SecondaryDark = Color(0xFF93C5FD)
+val SurfaceDark = Color(0xFF121212)
+val BackgroundDark = Color(0xFF0F172A)
+val OnSurfaceVariantDark = Color(0xFFCBD5E1)
