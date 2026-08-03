c1e359bdcaf19b934b78aba430bfd7515a266d23
HjAvila <hjavila86122@liceo.edu.ph>
2026-08-03 14:10:14 +0800

    Lab Activity 6 - Grocery List

--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -2,10 +2,20 @@ package com.example.myapplication.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light Green Theme
+val PrimaryLight = Color(0xFF2E7D32) // Forest Green
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryContainerLight = Color(0xFFA5D6A7) // Light Green
+val SecondaryLight = Color(0xFF1B5E20) // Darker Green
+val SurfaceLight = Color(0xFFF1F8E9) // Very Light Greenish White
+val BackgroundLight = Color(0xFFFFFFFF) // White
+val OnSurfaceVariantLight = Color(0xFF455A64)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Dark Green Theme
+val PrimaryDark = Color(0xFF81C784)
+val OnPrimaryDark = Color(0xFF003300)
+val PrimaryContainerDark = Color(0xFF1B5E20)
+val SecondaryDark = Color(0xFFA5D6A7)
+val SurfaceDark = Color(0xFF1B1B1B)
+val BackgroundDark = Color(0xFF121212)
+val OnSurfaceVariantDark = Color(0xFFCFD8DC)
